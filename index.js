3f39774c0e0789bb7158b19a9ceb69d58e902238
Egon <loliegon@gmail.com>
2021-11-20 19:55:56 +0100

    Made the code to look cleaner in the and new logic order

--- index.js
+++ index.js
@@ -5,20 +5,23 @@
 // if it is divisible by both 3 and 5, print "FizzBuzz"
 
 // if it is divisible by 7, print "Fish"
-// if it is divisible by 8, print "Bus"
+// if it is divisible by 9, print "Bus"
 
-for (let i=1; i<=100; i++){
-    if(i%3===0){
-       console.log(i,"Fizz")
-   }if(i%5===0){
-       console.log(i, "Buzz")
-   }if((i%3===0) && (i%5===0)){
-       console.log(i, "FizzBuzz")
-   }if(i%7===0){
-       console.log(i, "Fish")
-   }if(i%8===0){
-       console.log(i, "Bus")
-   }
+for(let i=1; i<=100; i++){
+    
+    if(i%3===0 && i%5===0 && i%7===0 && i%9===0){
+        console.log(FizzBuzzFishBus)
+   }else if(i%3===0 && i%5===0){
+       console.log("FizzBuzz")
+   }else if(i%3===0){
+       console.log("Fizz")
+   }else if(i%5===0){
+       console.log("Buzz")
+   }else if(i%7===0){
+       console.log("Fish")
+   }else if(i%9===0){
+       console.log("Bus")
+   }else{
        console.log(i)       
    }
-   
+}
