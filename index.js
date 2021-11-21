6507263273d271eceb1149ffbb7046294de30da6
Egon <loliegon@gmail.com>
2021-11-21 21:32:43 +0100

    Added the secon method with let result = ' '

--- index.js
+++ index.js
@@ -7,21 +7,42 @@
 // if it is divisible by 7, print "Fish"
 // if it is divisible by 9, print "Bus"
 
-for(let i=1; i<=100; i++){
+
+
+// THE FIRST WAY WITH IF-ELSE CONDITIONS
+
+// for(let i=1; i<=100; i++){
     
-    if(i%3===0 && i%5===0 && i%7===0 && i%9===0){
-        console.log(FizzBuzzFishBus)
-   }else if(i%3===0 && i%5===0){
-       console.log("FizzBuzz")
-   }else if(i%3===0){
-       console.log("Fizz")
-   }else if(i%5===0){
-       console.log("Buzz")
-   }else if(i%7===0){
-       console.log("Fish")
-   }else if(i%9===0){
-       console.log("Bus")
-   }else{
-       console.log(i)       
-   }
+//     if(i%3===0 && i%5===0 && i%7===0 && i%9===0){
+//         console.log(FizzBuzzFishBus)
+//    }else if(i%3===0 && i%5===0){
+//        console.log("FizzBuzz")
+//    }else if(i%3===0){
+//        console.log("Fizz")
+//    }else if(i%5===0){
+//        console.log("Buzz")
+//    }else if(i%7===0){
+//        console.log("Fish")
+//    }else if(i%9===0){
+//        console.log("Bus")
+//    }else{
+//        console.log(i)       
+//    }
+// }
+
+
+// THE SECOND WAY 
+
+for (let i=0; i<=100; i++){
+    
+    let result = ``
+
+    if(i%3===0) result += `Fizz`
+    if(i%5===0) result += `Buzz`
+    if(i%7===0) result += `Fish`
+    if(i%9===0) result += `Bus`
+    
+    if (result === ``) console.log(i)
+    else console.log(result) 
+
 }
